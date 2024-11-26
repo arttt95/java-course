f0d4db16d8d5b99aeeda6aa3d27a9bd394bb04e9
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-11-25 23:34:58 -0300

    25NOV24 - Aula 34 AS - Classes e objetos na teoria.

---
+++ app/src/java-course/b_classes_objetos_teoria.java
@@ -0,0 +1,17 @@
+class Scratch {
+    public static void main(String[] args) {
+
+        // Lower Camel Case
+        //jogoMarioKart
+
+        //Upper Camel Case
+        //JogoMarioKart
+
+        //Snake Case
+        //jogo_mario_kart
+
+        //Kebab Case
+        //jogo-mario-kart
+
+    }
+}
